b67aa47912be8ac9616a7c361c394e3237b28b60
AdrianoSantana <ans3@cin.ufpe.br>
2025-11-02 07:10:12 -0300

    Initial Commit

---
+++ TasksList/TasksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksListApp.swift
+//  TasksList
+//
+//  Created by Adriano Santana on 02/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
